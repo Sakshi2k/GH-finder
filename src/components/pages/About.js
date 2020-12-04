f6bc31d28d3f5d6a59fead3035e9ac82b9e84be1
Sakshi Choudhary <sakshi2k@yahoo.co.in>
2020-12-04 13:10:39 +0530

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,8 +8,8 @@ function About() {
       <br />
       <div style={{ marginLeft: '2rem' }}>
         <h6 className='lead' style={{ color: 'rgb(10, 141, 91)' }}>Welcome to Github Finder</h6>
-        <p className='lead'>This application helps you search Github Users using their username.</p>
-        <p className='lead'>You can view their summarised analysed data about any user too.</p>
+        <p className='lead'>This application helps you search Developers all over the world from Github Platform using their username/name.</p>
+        <p>You can also view their summarised data.</p>
         <p className='lead'>Version: 1.0</p>
         <p className='lead'>Made by<a rel="noopener noreferrer" target='_blank' href='https://meet-sakshichoudhary.herokuapp.com/'> Sakshi Choudhary</a></p>
       </div>
